0f90823c17a89a75741da35938d97b06c88ee796
pratyush <140149283+pratyush-6@users.noreply.github.com>
2024-03-07 15:09:12 +0530

    dbs

--- app/Models/minion.php
+++ app/Models/minion.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class minion extends Model
 {
     use HasFactory;
+    private $table="minions";
+    // protected $primary="id";
+    protected $guarded = [];
 }
